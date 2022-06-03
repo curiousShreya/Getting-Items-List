c0fbb09f3622ea3fc8f56b6a2839e90824d08a1d
Shreya Khandal <shreyakhandal.gmail.com>
2022-06-03 13:32:05 +0530

    redux-axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {Provider} from 'react-redux';
 import store from './redux/store';
 
@@ -7,10 +8,10 @@ import App from './App';
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+     <Provider store={store}>
+           <App />
+      </Provider>,
+      document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
